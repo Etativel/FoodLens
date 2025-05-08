bc42e4fbfd5e9ca283b098928f278dd0fb6f286b
Etativel <farhanmaulana4611@gmail.com>
2025-05-08 17:48:51 +0800

    feat: add router protection on the parent layout

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,13 +1,46 @@
 import "./Layout.css";
 import BottomNavigation from "./components/Navigation/BottomNavigation";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import UserProvider from "./contexts/UserProvider";
 
 function Layout() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://localhost:3000/auth/profile", {
+      credentials: "include",
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        throw new Error("Not authenticated");
+      })
+      .then(() => {
+        setIsAuthenticated(true);
+      })
+      .catch(() => {
+        setIsAuthenticated(false);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="sign-up" />;
+  }
   return (
-    <div className="h-screen relative overflow-hidden">
-      <Outlet className="content-wrapper" />
-      <BottomNavigation />
-    </div>
+    <UserProvider>
+      <div className="h-screen relative overflow-hidden">
+        <Outlet className="content-wrapper" />
+        <BottomNavigation />
+      </div>
+    </UserProvider>
   );
 }
 

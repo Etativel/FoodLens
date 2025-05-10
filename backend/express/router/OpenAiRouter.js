b1c923843b36718aae48e13e84182d6d1b713757
Etativel <farhanmaulana4611@gmail.com>
2025-05-10 11:20:59 +0800

    feat: add image upload on each controller

--- backend/express/router/OpenAiRouter.js
+++ backend/express/router/OpenAiRouter.js
@@ -2,9 +2,22 @@ const controller = require("../controller/OpenAiController");
 const express = require("express");
 const router = express.Router();
 const upload = require("../../config/multerConfig");
+const authenticateEither = require("../middleware/authEither");
 
-router.post("/food", controller.promptFoodInformation);
-// router.post("/food-vision", upload.single("image"), controller.openaiVision);
+router.post(
+  "/food",
+  authenticateEither,
+  upload.single("image"),
+  controller.promptFoodInformation
+);
+
+// Add is premium auth
+router.post(
+  "/food-vision",
+  authenticateEither,
+  upload.single("image"),
+  controller.openaiVision
+);
 // router.post(
 //   "/food-vision",
 //   upload.single("image"),

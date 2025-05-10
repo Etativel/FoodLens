98ee0bbffb4a8c2d685eb4721347f05f3281343c
Etativel <farhanmaulana4611@gmail.com>
2025-05-10 11:14:06 +0800

    feat: save scan history and  image on every scan

--- frontend/src/pages/Result/Result.jsx
+++ frontend/src/pages/Result/Result.jsx
@@ -1,7 +1,9 @@
 import { Navigate, useLocation } from "react-router-dom";
-import { useEffect, useState, useRef } from "react";
-import { Tag, ChevronDown, ChevronUp } from "lucide-react";
-import { formatText, toSnakeCase } from "../../utils";
+import { useEffect, useState, useRef, useContext } from "react";
+import { Tag } from "lucide-react";
+import { formatText, toSnakeCase, compressImage } from "../../utils";
+import UserContext from "../../contexts/createContext/UserContext";
+import IsAuthorize from "../../utils/isAuthorize";
 import {
   DietaryBadges,
   NutritionFacts,
@@ -82,6 +84,10 @@ export default function Results() {
   const [food, setFood] = useState(null);
   const { image, prediction } = state || {};
   const fetchGuard = useRef(false);
+  const { profile } = useContext(UserContext);
+  const [intakeStatus, setIntakeStatus] = useState("question");
+  // Store user's response (Yes/No)
+  const [userResponse, setUserResponse] = useState("");
   // const [isOpen, setIsOpen] = useState(false);
   // const [selected, setSelected] = useState("Default");
 
@@ -106,6 +112,16 @@ export default function Results() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [prediction, image, isPremium]);
 
+  useEffect(() => {
+    if (intakeStatus === "thanks") {
+      const timer = setTimeout(() => {
+        setIntakeStatus("hidden");
+      }, 3000); // Hide after 3 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [intakeStatus]);
+
   async function fetchOldModel(label) {
     try {
       const res = await fetch(`http://localhost:3000/food-api/food/${label}`, {
@@ -114,33 +130,43 @@ export default function Results() {
       });
       if (res.ok) {
         const json = await res.json();
+        console.log(json.data);
+
         setFood(json.data);
       } else if (res.status === 404) {
-        await fetchFromOpenAi(label);
+        await fetchFromOpenAi(label, image);
       } else {
         throw new Error(res.statusText);
       }
     } catch (err) {
       console.error("Fetch failed, falling back:", err);
-      await fetchFromOpenAi(label);
+      await fetchFromOpenAi(label, image);
     }
   }
-  async function fetchFromOpenAi(label) {
+
+  async function fetchFromOpenAi(label, imageDataUrl) {
     try {
+      const compressed = await compressImage(imageDataUrl);
+      // upload
+      const blob = await (await fetch(compressed)).blob();
+      const form = new FormData();
+      form.append("image", blob, "photo.jpg");
+      form.append("input", label);
+
       const resp = await fetch("http://localhost:3000/openai/food", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        // headers: { "Content-Type": "application/json" },
         credentials: "include",
-        body: JSON.stringify({ input: label }),
+        body: form,
       });
-      const { recipe } = await resp.json();
+      const { recipe, imageUrl } = await resp.json();
       const enriched = {
         ...recipe,
         name: formatText(label),
         predicted_name: label,
       };
       setFood(enriched);
-      await storeScanAndRecipe(enriched, "default", "");
+      await storeScanAndRecipe(enriched, "default", imageUrl);
     } catch (err) {
       console.error("OpenAI fallback failed", err);
     }
@@ -148,28 +174,7 @@ export default function Results() {
 
   async function fetchByVision(imageDataUrl) {
     try {
-      const img = new Image();
-      img.src = imageDataUrl;
-      await new Promise((r, e) => {
-        img.onload = r;
-        img.onerror = e;
-      });
-      const maxDim = 1024;
-      const canvas = document.createElement("canvas");
-      let { width, height } = img;
-      if (width > height && width > maxDim) {
-        height = (height * maxDim) / width;
-        width = maxDim;
-      } else if (height > maxDim) {
-        width = (width * maxDim) / height;
-        height = maxDim;
-      }
-      canvas.width = width;
-      canvas.height = height;
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(img, 0, 0, width, height);
-      const compressed = canvas.toDataURL("image/jpeg", 0.7);
-
+      const compressed = await compressImage(imageDataUrl);
       // upload
       const blob = await (await fetch(compressed)).blob();
       const form = new FormData();
@@ -194,6 +199,32 @@ export default function Results() {
       await storeScanAndRecipe(visionRecipe, "vision", imageUrl);
     } catch (err) {
       console.error("Vision fetch failed", err);
+    }
+  }
+
+  async function saveNewScan() {
+    try {
+      const compressed = await compressImage(image);
+      // upload
+      const blob = await (await fetch(compressed)).blob();
+      const form = new FormData();
+      form.append("image", blob, "photo.jpg");
+      form.append("scanMode", isPremium ? "vision" : "default");
+      form.append("userId", profile?.id);
+      form.append("recipeId", food.id);
+      const response = await fetch("http://localhost:3000/food/create/scan", {
+        method: "POST",
+        credentials: "include",
+        body: form,
+      });
+      if (!response.ok) {
+        console.log("Error, ", response.statusText);
+        return;
+      }
+      return;
+    } catch (err) {
+      console.log("Failed to create new scan, ", err);
+      return;
     }
   }
 
@@ -226,8 +257,28 @@ export default function Results() {
     return <Navigate to="/home" replace />;
   }
 
+  function handleIntakeResponse(answer) {
+    setUserResponse(answer);
+    setIntakeStatus("thanks");
+
+    if (answer === "Yes") {
+      saveNewScan();
+    }
+  }
+  const animationStyles = `
+    @keyframes fadeIn {
+      from { opacity: 0; transform: translateY(-10px); }
+      to { opacity: 1; transform: translateY(0); }
+    }
+    
+    .animate-fadeIn {
+      animation: fadeIn 0.5s ease-out forwards;
+    }
+  `;
+
   return (
     <>
+      <style>{animationStyles}</style>
       {!image ? (
         <div className="w-screen h-screen bg-white">No data</div>
       ) : (
@@ -239,6 +290,49 @@ export default function Results() {
               backgroundImage: `url(${image})`,
             }}
           ></div>
+          {/* Intake Log */}
+          {food && intakeStatus !== "hidden" && (
+            <div className="bg-neutral-800 flex flex-col px-3 pt-3 pb-3 transition-all duration-300 ease-in-out">
+              {intakeStatus === "question" ? (
+                <>
+                  <div className="block text-lg font-semibold text-white">
+                    Help us track your daily intake
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <div className="text-md text-neutral-300">
+                      Did you eat this today?
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => handleIntakeResponse("Yes")}
+                        className="w-10 bg-green-500 rounded-sm font-semibold text-white mx-2 hover:bg-green-600 transition-colors"
+                      >
+                        Yes
+                      </button>
+                      <button
+                        onClick={() => handleIntakeResponse("No")}
+                        className="w-10 bg-red-500 rounded-sm font-semibold text-white mx-2 hover:bg-red-600 transition-colors"
+                      >
+                        No
+                      </button>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <div className="animate-fadeIn flex flex-col items-center py-2">
+                  <div className="text-lg font-semibold text-white mb-1">
+                    Thank you for your response!
+                  </div>
+                  <div className="text-md text-neutral-300">
+                    {userResponse === "Yes"
+                      ? "We've logged this item to your daily intake."
+                      : "No problem, we won't add this to your tracking."}
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+
           <div className="px-3 py-3">
             <div>
               <div className="flex justify-between">

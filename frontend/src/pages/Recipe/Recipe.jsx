5984da59dc66e9c85095d7871a1cf8f388ec71ad
Etativel <farhanmaulana4611@gmail.com>
2025-05-14 11:37:04 +0800

    style recipe card

--- frontend/src/pages/Recipe/Recipe.jsx
+++ frontend/src/pages/Recipe/Recipe.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import { Search } from "lucide-react";
 
 function RecipeCard({ recipe }) {
-  const ingredients = Object.keys(recipe)
-    .filter((key) => key.startsWith("strIngredient") && recipe[key])
-    .map((ingredientKey, index) => ({
-      name: recipe[ingredientKey],
-      measure: recipe[`strMeasure${index + 1}`] || "",
-    }))
-    .filter((ing) => ing.name.trim() !== "");
+  const navigate = useNavigate();
+  function redirectRecipe(id) {
+    navigate(`/recipe/${id}`);
+  }
 
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden m-4 max-w-sm">
+    <div
+      onClick={() => redirectRecipe(recipe.idMeal)}
+      className="bg-white shadow-lg rounded overflow-hidden m-4  w-full"
+    >
       {recipe.strMealThumb && (
         <div className="relative">
           <img
@@ -20,64 +21,20 @@ function RecipeCard({ recipe }) {
             alt={recipe.strMeal}
             className="w-full h-48 object-cover"
           />
-          <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2">
+          <div className="absolute bottom-0 left-0 right-0 bg-neutral-800 bg-opacity-50 text-white p-2">
             <h2 className="text-xl font-bold truncate">{recipe.strMeal}</h2>
           </div>
         </div>
       )}
 
-      <div className="p-4">
-        <div className="mb-2">
+      <div className="p-2">
+        <div className="mb-1">
           <p className="text-gray-600">
             <strong>Category:</strong> {recipe.strCategory}
           </p>
           <p className="text-gray-600">
             <strong>Cuisine:</strong> {recipe.strArea}
           </p>
-        </div>
-
-        <div className="mb-2">
-          <h3 className="text-lg font-semibold mb-1">Key Ingredients</h3>
-          <ul className="text-sm text-gray-700 list-disc list-inside">
-            {ingredients.slice(0, 3).map((ing, index) => (
-              <li key={index}>
-                {ing.name} - {ing.measure}
-              </li>
-            ))}
-            {ingredients.length > 3 && (
-              <li className="text-gray-500">... and more</li>
-            )}
-          </ul>
-        </div>
-
-        <div className="flex justify-between items-center mt-4">
-          {(recipe.strYoutube || recipe.strSource) && (
-            <div className="flex space-x-2">
-              {recipe.strYoutube && (
-                <a
-                  href={recipe.strYoutube}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 hover:underline text-sm"
-                >
-                  Watch Video
-                </a>
-              )}
-              {recipe.strSource && (
-                <a
-                  href={recipe.strSource}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 hover:underline text-sm"
-                >
-                  Source
-                </a>
-              )}
-            </div>
-          )}
-          <button className="bg-blue-500 text-white px-3 py-1 rounded text-sm hover:bg-blue-600">
-            View Details
-          </button>
         </div>
       </div>
     </div>
@@ -87,32 +44,77 @@ function RecipeCard({ recipe }) {
 export default function Recipe() {
   const [filteredData, setFilteredData] = useState([]);
   const [filter, setFilter] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const queryValue = queryParams.get("query");
+
+    if (queryValue) {
+      setFilter(queryValue);
+      searchRecipe(queryValue);
+    }
+  }, [location.search]);
 
   async function searchRecipe(query) {
     if (!query) return;
     try {
+      setIsFetching(true);
+      // Protect this on router
       const response = await fetch(
         `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
       );
       if (!response.ok) {
         console.log("Failed to retrieve data,", response.statusText);
+        setIsFetching(false);
         return;
       }
       const data = await response.json();
       setFilteredData(data.meals || []);
+      setIsFetching(false);
+
       console.log(data);
     } catch (err) {
+      setIsFetching(false);
       console.log(err);
     }
   }
 
   const handleSearchSubmit = async () => {
-    if (location.pathname !== "/recipe") {
-      navigate("/recipe");
-    }
+    const newUrl = filter
+      ? `/recipe?query=${encodeURIComponent(filter)}`
+      : "/recipe";
+    navigate(newUrl, { replace: true });
     await searchRecipe(filter);
   };
+
+  if (isFetching) {
+    return (
+      <div className="h-full w-full flex justify-center items-center">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col h-screen lg:max-w-[500px] md:max-w-[500px]">
@@ -128,15 +130,18 @@ export default function Recipe() {
           </div>
         </div>
 
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center mt-8 mb-20">
           {filteredData.map((recipe) => (
             <RecipeCard key={recipe.idMeal} recipe={recipe} />
           ))}
         </div>
 
         {filteredData.length === 0 && (
-          <div className="text-white text-center p-4">
-            No recipes found. Try another search.
+          <div className="text-white text-center p-4 transform translate-y-20 flex flex-col items-center justify-center">
+            <Search size={48} className="text-gray-200 mb-3" />
+            <p className="text-lg font-semibold">
+              No recipes found. Try another search.
+            </p>
           </div>
         )}
       </div>

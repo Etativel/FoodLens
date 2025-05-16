8035393eba90ea92f117c76218e7e850056272b7
Etativel <farhanmaulana4611@gmail.com>
2025-05-16 18:27:09 +0800

    feat: add new layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import React, { Suspense } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import UserProvider from "./contexts/UserProvider";
 
 import "./index.css";
 import Home from "./pages/Home/Home.jsx";
@@ -11,38 +10,37 @@ import Settings from "./pages/Settings/Settings.jsx";
 import Calories from "./pages/Calories/Calories.jsx";
 import Result from "./pages/Result/Result.jsx";
 import Recipe from "./pages/Recipe/Recipe.jsx";
-import Layout from "./Layout.jsx";
+import BottomNavLayout from "./BottomnavLayout.jsx";
+import AppLayout from "./AppLayout.jsx";
 import RecipeDetails from "./components/Recipe/RecipeDetails.jsx";
 import ScanDetails from "./pages/ScanDetails/ScanDetails.jsx";
+import UpdateField from "./pages/UpdateField/UpdateField.jsx";
 // import Processing from "./components/Processing/Processing.jsx";
 import Onboarding from "./pages/Onboarding/Onboarding.jsx";
 import SignIn from "./pages/Auth/SingIn.jsx";
 import Signup from "./pages/Auth/SignUp.jsx";
 
 const router = createBrowserRouter([
+  { path: "/sign-in", element: <SignIn /> },
+  { path: "/sign-up", element: <Signup /> },
   {
-    path: "/sign-in",
-    element: <SignIn />,
-  },
-  {
-    path: "/sign-up",
-    element: <Signup />,
-  },
-  {
-    path: "/onboarding",
-    element: <Onboarding />,
-  },
-  { path: "/results", element: <Result /> },
-  { path: "/recipe/:recipeId", element: <RecipeDetails /> },
-  { path: "/scan/:foodId", element: <ScanDetails /> },
-  {
-    element: <Layout />,
+    element: <AppLayout />, // provides context
     children: [
-      { path: "/home", element: <Home /> },
-      { path: "/calories", element: <Calories /> },
-      { path: "/my-food", element: <MyFood /> },
-      { path: "/settings", element: <Settings /> },
-      { path: "/recipe", element: <Recipe /> },
+      {
+        element: <BottomNavLayout />, // shows BottomNav
+        children: [
+          { path: "/home", element: <Home /> },
+          { path: "/calories", element: <Calories /> },
+          { path: "/my-food", element: <MyFood /> },
+          { path: "/settings", element: <Settings /> },
+          { path: "/recipe", element: <Recipe /> },
+        ],
+      },
+      { path: "/onboarding", element: <Onboarding /> },
+      { path: "/results", element: <Result /> },
+      { path: "/recipe/:recipeId", element: <RecipeDetails /> },
+      { path: "/scan/:foodId", element: <ScanDetails /> },
+      { path: "/settings/:field", element: <UpdateField /> },
     ],
   },
 ]);
@@ -50,9 +48,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Suspense fallback="">
-      <UserProvider>
-        <RouterProvider router={router} />
-      </UserProvider>
+      <RouterProvider router={router} />
     </Suspense>
   </StrictMode>
 );

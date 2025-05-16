bd6ce1b9413866eb13557d2cbf0087febd6a9f86
Etativel <farhanmaulana4611@gmail.com>
2025-05-16 10:44:57 +0800

    redirect to scandetails when user clicking the food card

--- frontend/src/pages/MyFood/MyFood.jsx
+++ frontend/src/pages/MyFood/MyFood.jsx
@@ -4,23 +4,35 @@ import { ClipboardList } from "lucide-react";
 
 import { useState, useEffect } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function FoodCard({ food }) {
+function FoodCard({ food, redirectToDetails }) {
   const [showNutrient, setShowNutrient] = useState(false);
 
   return (
     <div className="flex gap-4 items-start">
       <div
+        onClick={() =>
+          redirectToDetails(food.recipe.id, food.recipe, food.imageUrl)
+        }
         className="h-20 w-[150px] min-w-[100px] bg-cover bg-center rounded-lg mt-2"
         style={{ backgroundImage: `url(${food.imageUrl})` }}
       ></div>
 
       <div className="flex flex-col justify-center text-white max-w-xs overflow-hidden">
         <div className="flex justify-between">
-          <span className="text-lg font-semibold truncate pr-2">
+          <span
+            onClick={() =>
+              redirectToDetails(food.id, food.recipe, food.imageUrl)
+            }
+            className="text-lg font-semibold truncate pr-2"
+          >
             {food.recipe.name}
           </span>
-          <button onClick={() => setShowNutrient((prev) => !prev)}>
+          <button
+            aria-label="show dropdown"
+            onClick={() => setShowNutrient((prev) => !prev)}
+          >
             {showNutrient ? (
               <ChevronUp className="w-5 h-5 text-white" strokeWidth={2.5} />
             ) : (
@@ -29,7 +41,10 @@ function FoodCard({ food }) {
           </button>
         </div>
 
-        <span className="text-sm text-gray-300">
+        <span
+          onClick={() => redirectToDetails(food.id, food.recipe, food.imageUrl)}
+          className="text-sm text-gray-300"
+        >
           <p>{food.recipe.summary}</p>
           <p className="text-sm text-neutral-400">
             {new Date(food.scannedAt).toLocaleString("en-US", {
@@ -89,6 +104,12 @@ function FoodCard({ food }) {
 }
 
 function FoodContent({ foods }) {
+  const navigate = useNavigate();
+  function redirectToDetails(id, recipe, image) {
+    navigate(`/scan/${id}`, {
+      state: { recipe, image },
+    });
+  }
   return (
     <div className="flex mx-3 flex-col mb-25">
       <div className="text-white mb-2 text-lg font-semibold mt-10">
@@ -96,7 +117,11 @@ function FoodContent({ foods }) {
       </div>
       <div className="flex flex-col gap-2">
         {foods.map((food, idx) => (
-          <FoodCard key={idx} food={food} />
+          <FoodCard
+            key={idx}
+            food={food}
+            redirectToDetails={redirectToDetails}
+          />
         ))}
       </div>
     </div>

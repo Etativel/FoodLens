04dc895cdf7d151122d35966adc740a88f84efed
Etativel <farhanmaulana4611@gmail.com>
2025-05-17 17:25:08 +0800

    update port

--- backend/python/app.py
+++ backend/python/app.py
@@ -115,4 +115,6 @@ def predict():
     return jsonify({"predicted_label": label}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

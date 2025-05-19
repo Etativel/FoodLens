703e73d8758538b7d03fd086fb854d6157119dac
Etativel <farhanmaulana4611@gmail.com>
2025-05-19 18:27:00 +0800

    feat: add LoginCode table

---
+++ backend/express/prisma/migrations/20250519071047_add_login_code/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "LoginCode" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "LoginCode_pkey" PRIMARY KEY ("id")
+);
